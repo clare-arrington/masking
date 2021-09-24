e797b9b22c4e719a46d5da9eb9674a1e04cf1f7c
Clare Arrington <arrinj@rpi.edu>
2021-09-24 12:12:34 -0400

    Adjustment to numerical settings; resorted other settings

--- wsi/WSISettings.py
+++ wsi/WSISettings.py
@@ -8,20 +8,16 @@ WSISettings = namedtuple('WSISettings', ['n_represents', 'n_samples_per_rep', 'c
 
 DEFAULT_PARAMS = WSISettings(
     n_represents=15,
-    n_samples_per_rep=20,
-    cuda_device=0,
-    disable_lemmatization=False,
-    disable_tfidf=False,
-
-    ## Patterns
-    # (pattern,weight): each of these patterns will produce a prediction state.
-    # the weighted sum of them will be matmul'ed for a distribution over substitutes
+    n_samples_per_rep=50,
 
     max_number_senses=15,
-    min_sense_instances=15,
+    min_sense_instances=50,
     # sense clusters that dominate less than this number of samples
     # would be remapped to their closest big sense
 
+    cuda_device=0,
+    disable_lemmatization=False,
+    disable_tfidf=False,
     max_batch_size=10,
     prediction_cutoff=30522,
     bert_model='bert-base-uncased'

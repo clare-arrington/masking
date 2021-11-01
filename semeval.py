d647abcd01c2174bf92d736d71ea23af10b9d430
Clare Arrington <arrinj@rpi.edu>
2021-11-01 00:32:03 -0400

    Shuffled functions around a bit

--- semeval.py
+++ semeval.py
@@ -2,10 +2,11 @@
 from base_wsi import get_data, make_predictions, perform_clustering, create_sense_sentences 
 
 ## Pull data
-corpus_name = 'ccoha1'
+corpus_name = 'ccoha2'
 # corpus_name = None
 sentence_path = '/home/clare/Data/corpus_data/semeval/subset/target_sentences.csv'
 target_path = '/home/clare/Data/corpus_data/semeval/subset/target_information.csv'
+output_path = f'/home/clare/Data/masking_results/semeval/{corpus_name}'
 
 if corpus_name == 'ccoha1':
     dataset_desc = 'SemEval 2020 Task \nCCOHA 1: 1810 - 1860'
@@ -13,32 +14,34 @@ elif corpus_name == 'ccoha2':
     dataset_desc = 'SemEval 2020 Task \nCCOHA 2: 1960 - 2010'
 else:
     dataset_desc = 'SemEval 2020 Task \nCCOHA 1 and 2'
+    corpus_name = None
 
-target_data = get_data(sentence_path, target_path, corpus_name)
-targets = [[target] for target in target_data.target.unique()]
+#%%
+target_data = get_data(sentence_path, target_path, 
+    corpus_name, limit_occurences=False)
+
+with open('/home/clare/Data/corpus_data/semeval/targets.txt') as fin:
+    targets = fin.read().split()
+    targets = [target.split('_')[0] for target in targets]
+
+# targets = [[target] for target in target_data.target.unique()]
 
 # targets = [
 #     'bit', 'face', 'gas', 'head', 'lane',
 #     'part', 'plane', 'record', 'word'] 
-# targets = [[target] for target in targets]
+targets = [[target] for target in targets]
 
 #%%
-## Set training info 
-if corpus_name is None:
-    corpus_name = 'all'
+# make_predictions(
+#     target_data, dataset_desc, output_path, 
+#     targets, subset_num=5000)
 
-output_path = f'/home/clare/Data/masking_results/semeval/{corpus_name}_extra'
-logging_file = f'{output_path}/targets.log'
-
-#%%
-make_predictions(
-    target_data, dataset_desc, output_path, 
-    targets, subset_num=5000)
-
-perform_clustering(
-    target_data, targets, dataset_desc, 
-    output_path)
+# perform_clustering(
+#     target_data, targets, dataset_desc, 
+#     output_path)
 
 print('Done!')
 # %%
-create_sense_sentences(sentence_path, output_path)
+create_sense_sentences(sentence_path, target_path, output_path)
+
+# %%

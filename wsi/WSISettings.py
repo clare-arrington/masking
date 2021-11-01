f22d8159d18fc9a7a6e28f36404563e699a756b0
Clare Arrington <arrinj@rpi.edu>
2021-11-01 00:24:50 -0400

    Removed a few old settings

--- wsi/WSISettings.py
+++ wsi/WSISettings.py
@@ -1,15 +1,12 @@
 from collections import namedtuple
 
-WSISettings = namedtuple('WSISettings', ['n_represents', 'n_samples_per_rep', 'cuda_device',
+WSISettings = namedtuple('WSISettings', ['cuda_device', 'max_number_senses',
                                          'disable_tfidf', 'disable_lemmatization', 
                                          'min_sense_instances', 'bert_model',
-                                         'max_batch_size', 'prediction_cutoff', 'max_number_senses'
+                                         'max_batch_size', 'prediction_cutoff' 
                                          ])
 
 DEFAULT_PARAMS = WSISettings(
-    n_represents=15, ##TODO: where is this used?
-    n_samples_per_rep=50,
-
     max_number_senses=15,
     min_sense_instances=50,
     # sense clusters that dominate less than this number of samples

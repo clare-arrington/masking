4b87c9338b77930c096cccfdbf2881b6083ee6eb
Clare Arrington <arrinj@rpi.edu>
2021-11-27 23:56:15 -0500

    Handle sentences with multiple targets better

--- base_wsi.py
+++ base_wsi.py
@@ -3,6 +3,7 @@ from wsi.lm_bert import LMBert, trim_predictions
 from wsi.WSISettings import DEFAULT_PARAMS, WSISettings
 from wsi.wsi_clustering import cluster_predictions
 
+from ast import literal_eval
 from typing import List
 from datetime import datetime
 from dateutil import tz
@@ -11,36 +12,95 @@ import pandas as pd
 from glob import glob
 import time
 
-## TODO: sent_id is messed up. Ahhhhhhhhhhh
-def get_data(sentence_path, target_path, corpus_name=None, limit_occurences=True):
-    sentence_data = pd.read_csv(sentence_path, usecols=['corpus', 'sent_id'])
-    sentence_data.set_index(['sent_id'], inplace=True)
+def get_data(target_path, sentence_path=None, corpus_name=None, 
+             occurence_limit=None, length_minimum=25):
+    if 'csv' in target_path:
+        target_data = pd.read_csv(target_path)
+    elif 'pkl' in target_path:
+        target_data = pd.read_pickle(target_path)
+    
+    print(f'{len(target_data)} target instances pulled')
 
-    target_data = pd.read_csv(target_path)
-    target_data = target_data.join(sentence_data, on='sent_id')
-    print(f'{len(target_data)} total target instances pulled')
+    if sentence_path:
+        if 'csv' in sentence_path:
+            sentence_data = pd.read_csv(sentence_path, usecols=['corpus', 'sent_id'])
+            sentence_data.set_index(['sent_id'], inplace=True)
+        elif 'pkl' in sentence_path:
+            sentence_data = pd.read_pickle(sentence_path)
+            sentence_data.drop(columns=['sentence', 'processed_sentence', 'word_index_sentence'], inplace=True)
+        target_data = target_data.join(sentence_data, on='sent_id')
 
-    if corpus_name is not None:
-        target_data = target_data[target_data.corpus == corpus_name]
-        print(f'{len(target_data)} instances within {corpus_name}')
+        if corpus_name is not None:
+            target_data = target_data[target_data.corpus == corpus_name]
+            print(f'{len(target_data)} instances within {corpus_name}')
 
-    ## Only picking rows with 1 target
-    # TODO: this isn't ideal; the problem being avoided is making sure that no sentence with mult. targets
-    # only gets sense induction for one of them
-    if limit_occurences:
+    vc = target_data.target.value_counts()
+    print(f'{len(vc)} targets before anything removed')
+    targets = vc[vc >= 25].index
+    target_data = target_data[target_data.target.isin(targets)]
+    print(f'{len(target_data)} after insufficient targets removed')
+
+    if length_minimum:
+        ids = target_data[target_data.length <= 25].sent_id.unique()
+        target_data = target_data[~target_data.sent_id.isin(ids)]
+        print(f'{len(target_data)} after length minimum applied')
+
+    if occurence_limit:
         vc = target_data.sent_id.value_counts()
-        ids = vc[vc == 1].index
+        ids = vc[vc <= occurence_limit].index
         target_data = target_data[target_data.sent_id.isin(ids)]
         print(f'{len(target_data)} after limit applied')
 
-    target_data.formatted_sentence = target_data.formatted_sentence.apply(eval)
+    # target_data.formatted_sentence = target_data.formatted_sentence.apply(literal_eval)
 
     return target_data
 
-def pull_rows(data, targets, subset_num):
+def pull_rows(data, subset_num):
+    target_rows = {}
+    sent_ids = set()
+    vc = data.target.value_counts(ascending=True)
+    for target in vc.index:        
+        # If the target is the only thing in the sent, we'll get nonsense. 
+        ## TODO: issue if we disclude based on length here :/
+        data_subset = data[(data.target == target) & (data.length >= 25)]
+        print(target, len(data_subset))
+
+        ## If too big, skip for now. 
+        if subset_num is not None and (len(data_subset) > subset_num):
+            continue
+        else:
+            ## Save the ids of all samples in case they overlap, 
+            ## since one sentence can have multiple targets, we want to include them all.
+
+            target_rows[target] = data_subset
+            # before = len(sent_ids)
+            sent_ids.update(data_subset.sent_id)  
+            # print(f'\t{len(sent_ids) - before} ids added')
+
+    ## Now we go back through and resample those that were too big
+    ## now that all the samples have been accounted for.
+    for target in vc.index:
+        if target not in target_rows:
+            print(target)
+
+            data_subset = data[(data.target == target) & (data.length >= 25)]
+            already_sampled = sum(data_subset.sent_id.isin(sent_ids))
+            print(target)
+            print(f'\t{already_sampled} already sampled')
+
+            sample_subset = data_subset[~data_subset.sent_id.isin(sent_ids)]
+            sample_num = max(0, subset_num - already_sampled)
+            sent_ids.update(sample_subset.sample(sample_num).index)
+            print(sample_num)
+
+            target_rows[target] = data_subset[data_subset.sent_id.isin(sent_ids)]
+            print(target, len(data_subset))
+
+    return target_rows
+
+def pull_corpus_rows(data, targets, subset_num):
     target_rows = {}
     for target in targets:
-        # data[data.formatted_sentence.apply(lambda s: len(s[0]) == 0 or len(s[2]) == 0)]
         # If the target is the only thing in the sent, we'll get nonsense. 
         data_subset = data[(data.target.isin(target)) & (data.length >= 25)]
         if subset_num:
@@ -54,8 +114,7 @@ def pull_rows(data, targets, subset_num):
 
             data_subset = pd.concat(samples)
         
-        num_rows = len(data_subset)
-        target_rows[target[0]] = (data_subset, num_rows)
+        target_rows[target[0]] = data_subset
 
     return target_rows
 
@@ -76,7 +135,7 @@ def make_predictions(
     targets: List[str],
     subset_num=None,
     resume_predicting=False
-):
+    ):
 
     ## Pull settings from file
     settings = DEFAULT_PARAMS._asdict()
@@ -94,27 +153,30 @@ def make_predictions(
             print(dataset_desc, file=flog)
             print(f'\n{len(data)} rows loaded', file=flog)
             print(f'{len(targets)} targets loaded\n', file=flog)
-
-    if resume_predicting:
+    else:
         already_predicted = glob(f'{output_path}/predictions/*.pkl')
         skip_targets = [path.split('/')[-1][:-4] for path in already_predicted]
-        
+        print(f'{len(skip_targets)} targets already predicted')
+
         remove_targets = []
         for target in targets:
             if target[0] in skip_targets:
                 remove_targets.append(target)
+        print(f'Removing {len(remove_targets)} targets')
 
         for target in remove_targets:
             targets.remove(target)
+        print(f'{len(targets)} targets going to be clustered')
 
-    target_rows = pull_rows(data, targets, subset_num)
+    target_rows = pull_rows(data, subset_num)
 
     for n, target_alts in enumerate(sorted(targets)):
         # break
         target = target_alts[0]
         print(f'\n{n+1} / {len(targets)} : {" ".join(target_alts)}')
 
-        data_subset, num_rows = target_rows[target]
+        data_subset = target_rows[target]
+        num_rows = len(data_subset)
 
         with open(logging_file, 'a') as flog:
             print('====================================\n', file=flog)
@@ -135,12 +197,13 @@ def make_predictions(
             print(f'\tPredictions saved')
 
 #%%
-def perform_clustering(
-        data: pd.DataFrame,
-        targets: List[str],
-        dataset_desc: str, 
-        output_path: str
-        ):
+def make_clusters(
+    target_data: pd.DataFrame,
+    dataset_desc: str,
+    output_path: str,
+    targets: List[str],
+    resume_clustering: bool = False
+    ):
 
     ## Pull settings from file
     settings = DEFAULT_PARAMS._asdict()
@@ -150,11 +213,25 @@ def perform_clustering(
     Path(f'{output_path}/summaries').mkdir(parents=True, exist_ok=True)
     logging_file = f'{output_path}/clustering.log'
 
-    # target_paths = glob(f'{output_path}/predictions/*.pkl')
-
     ## Start the new logging file for this run
-    with open(logging_file, 'w') as flog:
-        print(dataset_desc, file=flog)
+    if not resume_clustering:
+        with open(logging_file, 'w') as flog:
+            print(dataset_desc, file=flog)
+    else:
+        all_sense_data = pd.read_pickle(f'{output_path}/target_sense_labels.pkl')
+        skip_targets = all_sense_data.target.unique()
+        print(f'{len(skip_targets)} targets already clustered')
+        
+        remove_targets = []
+        for target in targets:
+            if target[0] in skip_targets:
+                remove_targets.append(target)
+        print(f'Removing {len(remove_targets)} targets')
+
+        for target in remove_targets:
+            targets.remove(target)
+
+        print(f'{len(targets)} targets going to be clustered')
 
     sense_data = []
     for n, target_alts in enumerate(sorted(targets)):
@@ -181,7 +258,6 @@ def perform_clustering(
                 end = time.time()
                 print(f'\t    End time : {convert_to_local(end)}', file=flog)
                 print(f'\t    End time : {convert_to_local(end)}')
-
             else:
                 ## We don't want to cluster a target that is too small
                 print('\tSkipping WSI; not enough rows\n', file=flog)
@@ -189,16 +265,22 @@ def perform_clustering(
                     '0' : list(predictions.index)
                 }
 
-            print('\nCluster results')
+            print('\n\tCluster results')
             for sense, cluster in sense_clusters.items():
                 print(f'\t{sense} : {len(cluster)}', file=flog)
                 print(f'\t{sense} : {len(cluster)}')
 
-        sense_info = save_results(data, dataset_desc, target, sense_clusters, output_path)
+        sense_info = save_results(target_data, dataset_desc, target, sense_clusters, output_path)
         sense_data.append(sense_info)
 
-    sense_data = pd.concat(sense_data)
-    sense_data.to_csv(f'{output_path}/target_sense_labels.csv', index=False)
+    if len(sense_data) > 0:
+        sense_data = pd.concat(sense_data)
+        sense_data.set_index('word_index', inplace=True)
+        
+        if resume_clustering:
+            sense_data = pd.concat([all_sense_data, sense_data])        
+        
+        sense_data.to_pickle(f'{output_path}/target_sense_labels.pkl')
 # %%
 def save_results(data, dataset_desc, target, sense_clusters, output_path):
     cluster_data = []
@@ -229,33 +311,43 @@ def save_results(data, dataset_desc, target, sense_clusters, output_path):
 
     return cluster_data
 
-def create_sense_sentences(sentence_path, target_path, output_path):
-    target_data = pd.read_csv(
-        f'{output_path}/target_sense_labels.csv', 
-        usecols=['word_index', 'target', 'cluster'])
-    target_data.set_index('word_index', inplace=True) ## maybe this default
+def create_sense_sentences(sentence_path, output_path):
+    target_data = pd.read_pickle(
+        f'{output_path}/target_sense_labels.pkl')
     print(f'{len(target_data)} targets predicted')
+
+    targets = list(target_data.target.unique())
+    print(f'{len(targets)} targets selected')
+    ids = target_data.sent_id.unique()
+    print(f'{len(ids)} unique sentences')
 
     ## TODO: somehow there are terms that don't have a sent_id;
     ## how did that even happen?
-    good_target_data = pd.read_csv(target_path,
-        usecols=['word_index', 'sent_id'])
-    good_target_data.set_index('word_index', inplace=True) ## maybe this default
+    # good_target_data = pd.read_csv(target_path,
+    #     usecols=['word_index', 'sent_id'])
+    # good_target_data.set_index('word_index', inplace=True) ## maybe this default
     
-    target_data = target_data.join(good_target_data)
-    target_data.dropna(inplace=True)
-    target_data.sent_id = target_data.sent_id.astype(int)
-    print(f'{len(target_data)} targets after nulls removed')
+    # target_data = target_data.join(good_target_data)
+    # target_data.dropna(inplace=True)
+    # target_data.sent_id = target_data.sent_id.astype(int)
+    # print(f'{len(target_data)} targets after nulls removed')
 
-    ids = target_data.sent_id.unique()
-    print(f'{len(ids)} unique sentences')
-    targets = list(target_data.target.unique())
-    print(f'{len(targets)} targets selected')
+    if 'csv' in sentence_path:
+        sentence_data = pd.read_csv(sentence_path, usecols=['sent_id', 'word_index_sentence'])
+        sentence_data.word_index_sentence = sentence_data.word_index_sentence.apply(eval)
+        # sentence_data.set_index('sent_id', inplace=True) ## maybe this default
+    elif 'pkl' in sentence_path:
+        sentence_data = pd.read_pickle(sentence_path)
+        sentence_data.drop( columns=['corpus','sentence', 'processed_sentence'],
+                            inplace=True)
 
-    sentence_data = pd.read_csv(sentence_path, usecols=['sent_id', 'word_index_sentence'])
-    sentence_data.word_index_sentence = sentence_data.word_index_sentence.apply(eval)
-    sentence_data.set_index('sent_id', inplace=True) ## maybe this default
-    sentence_data = sentence_data.iloc[ids]
+    good_ids = sentence_data.index.intersection(ids)
+    if len(good_ids) < len(ids):
+        bad_ids = set(ids) - set(good_ids)
+        print(f'Removing {len(bad_ids)} sents from these targets:')
+        print(target_data[target_data.sent_id.isin(bad_ids)].target.unique())
+        ids = good_ids
+    sentence_data = sentence_data.loc[ids]
 
     sense_sents = []
     num_bad = 0
@@ -278,7 +370,7 @@ def create_sense_sentences(sentence_path, target_path, output_path):
                 sense_sent.append(sense)
 
             else:
-                print(f'Bad! {sent_id}')
+                print(f'Bad! {sent_id} - {word}')
                 num_bad += 1
                 add_sent = False
                 break
@@ -292,4 +384,4 @@ def create_sense_sentences(sentence_path, target_path, output_path):
     sense_data = pd.DataFrame(sense_sents, columns=['sent_id', 'sense_sentence'])
     sense_data.set_index('sent_id', inplace=True) 
 
-    sense_data.to_csv(f'{output_path}/sense_sentences.csv')
+    sense_data.to_pickle(f'{output_path}/sense_sentences.pkl')

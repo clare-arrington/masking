d810cbd1984f7c894067a3fef88bf7eab2ef1265
Clare Arrington <arrinj@rpi.edu>
2021-11-27 23:56:48 -0500

    Redone to interface with new base structures

--- covid_news.py
+++ covid_news.py
@@ -1,33 +1,34 @@
 #%%
-from base_wsi import get_data, make_predictions, cluster_predictions
+from base_wsi import get_data, make_predictions, make_clusters, create_sense_sentences
+
+def run_slice_predictions(input_path, output_path, corpus_name, slice_num, resume):
+    data_path = f'{input_path}/{corpus_name}/slice_{slice_num}'
+    sentence_path = f'{data_path}/target_sentences.pkl'
+    target_path = f'{data_path}/target_information.pkl'
+    masking_path = f'{output_path}/{corpus_name}/slice_{slice_num}'
+
+    dataset_desc = f'{corpus_name.capitalize()} News Corpus'
+    target_data = get_data(target_path, occurence_limit=25)
+    targets = [[target] for target in target_data.target.unique() if target != 'coronavirus']
+    print(f'{len(targets)} targets for masking')
+
+    make_predictions(target_data, dataset_desc, masking_path, targets.copy(),
+                    subset_num=10000, resume_predicting=resume)
+
+    make_clusters(target_data, dataset_desc, masking_path, 
+                targets, resume_clustering=resume)
+    create_sense_sentences(sentence_path, masking_path)
 
 ## Pull data
-corpus_name = 'unreliable'
-data_path = '/data/arrinj/corpus_data/covid/subset'
-sentence_path = f'{data_path}/target_sentences.csv'
-target_path = f'{data_path}/target_information.csv'
-output_path = f'/data/arrinj/masking_results/covid/{corpus_name}'
+input_path = '/data/arrinj/corpus_data/news/subset'
+output_path = '/data/arrinj/masking_results/news'
 
-if 'reliable' in corpus_name:
-    dataset_desc = f'{corpus_name.capitalize()} COVID Corpus'
-    corpus_name = f'nela_{corpus_name}'
-else:
-    dataset_desc = 'Both reliable and unreliable COVID Corpora'
-    corpus_name = None
+for corpus_name in ['mainstream']: # 'alternative'
+    for slice_num in range(0, 6):
+        run_slice_predictions(input_path, output_path, corpus_name, slice_num, resume=False)
+        print(f'{corpus_name.capitalize()} - slice {slice_num} done!\n\n')
 
-#%%
-target_data = get_data(sentence_path, target_path, corpus_name)
+# corpus_name = 'mainstream'
+# slice_num = 0
 
-targets = [
-    'corona', 'virus', 'covid', 'coronavirus', 'case', 'pandemic', 'crisis' 
-    'mask', 'lockdown', 'quarantine', 'normal', 'death', 'trump']
-targets = [[t] for t in targets]
-
-#%%
-make_predictions(target_data, dataset_desc, output_path, targets,
-subset_num=5000, resume_predicting=False)
-
-# cluster_predictions(dataset_desc, target_data, output_path, targets)
-
-print('Done!')
 # %%

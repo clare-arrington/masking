cab0cdec7aaf3ce75f8b2cdac1814ff01789e14e
Clare Arrington <arrinj@rpi.edu>
2021-11-29 01:07:18 -0500

    Updating to new methods

--- semeval.py
+++ semeval.py
@@ -1,47 +1,42 @@
 #%%
-from base_wsi import get_data, make_predictions, perform_clustering, create_sense_sentences 
+from base_wsi import get_data, make_predictions, make_clusters, create_sense_sentences
 
 ## Pull data
-corpus_name = 'ccoha2'
-# corpus_name = None
-sentence_path = '/home/clare/Data/corpus_data/semeval/subset/target_sentences.csv'
-target_path = '/home/clare/Data/corpus_data/semeval/subset/target_information.csv'
+corpus_name = '1800s'
+input_path = '/home/clare/Data/corpus_data/semeval/subset'
+sentence_path = f'{input_path}/target_sentences.pkl'
+target_path = f'{input_path}/target_information.pkl'
 output_path = f'/home/clare/Data/masking_results/semeval/{corpus_name}'
 
-if corpus_name == 'ccoha1':
+if corpus_name == '1800s':
     dataset_desc = 'SemEval 2020 Task \nCCOHA 1: 1810 - 1860'
-elif corpus_name == 'ccoha2':
+elif corpus_name == '2000s':
     dataset_desc = 'SemEval 2020 Task \nCCOHA 2: 1960 - 2010'
 else:
-    dataset_desc = 'SemEval 2020 Task \nCCOHA 1 and 2'
+    dataset_desc = 'SemEval 2020 Task \n1800s (ccoha1) and 2000s (ccoha2)'
     corpus_name = None
 
 #%%
-target_data = get_data(sentence_path, target_path, 
-    corpus_name, limit_occurences=False)
+target_data = get_data(target_path, sentence_path, corpus_name)
+targets = [[target] for target in target_data.target.unique()]
 
-with open('/home/clare/Data/corpus_data/semeval/targets.txt') as fin:
-    targets = fin.read().split()
-    targets = [target.split('_')[0] for target in targets]
+# targets = [['ball'], ['face']]
 
-# targets = [[target] for target in target_data.target.unique()]
-
-# targets = [
-#     'bit', 'face', 'gas', 'head', 'lane',
-#     'part', 'plane', 'record', 'word'] 
-targets = [[target] for target in targets]
+# with open('/home/clare/Data/corpus_data/semeval/targets.txt') as fin:
+#     targets = fin.read().split()
+#     targets = [[target.split('_')[0]] for target in targets]
 
 #%%
-# make_predictions(
-#     target_data, dataset_desc, output_path, 
-#     targets, subset_num=5000)
+make_predictions(
+    target_data, targets, 
+    dataset_desc, output_path)
 
-# perform_clustering(
-#     target_data, targets, dataset_desc, 
-#     output_path)
+make_clusters(
+    target_data, targets, 
+    dataset_desc, output_path, plot_clusters=True)
 
 print('Done!')
 # %%
-create_sense_sentences(sentence_path, target_path, output_path)
+create_sense_sentences(sentence_path, output_path)
 
 # %%

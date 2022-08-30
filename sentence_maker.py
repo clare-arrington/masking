b3a5b086f9eec1428a18319e52e6efb49e682934
Clare Arrington <arrinj@rpi.edu>
2022-08-30 11:28:16 -0400

    Minor renamings

--- sentence_maker.py
+++ sentence_maker.py
@@ -4,14 +4,11 @@ from tqdm import tqdm
 
 def get_sentence_data(sentence_path):
     if 'csv' in sentence_path:
-        sentence_data = pd.read_csv(sentence_path, usecols=['sent_id', 'word_index_sentence'])
-        sentence_data.word_index_sentence = sentence_data.word_index_sentence.apply(eval)
-        # sentence_data.set_index('sent_id', inplace=True) ## maybe this default
+        sentence_data = pd.read_csv(sentence_path, usecols=['sent_idx', 'word_idx_sent'])
+        sentence_data.word_idx_sent = sentence_data.word_idx_sent.apply(eval)
+        # sentence_data.set_index('sent_idx', inplace=True) ## maybe this default
     elif 'pkl' in sentence_path:
         sentence_data = pd.read_pickle(sentence_path)
-        sentence_data.drop( columns=['corpus','sentence'],
-                            inplace=True)
-
     return sentence_data
 
 def process_sentences(sentence_data, target_data, targets, ids):
@@ -22,14 +19,14 @@ def process_sentences(sentence_data, target_data, targets, ids):
     if len(good_ids) < len(ids):
         bad_ids = set(ids) - set(good_ids)
         print(f'Removing {len(bad_ids):,} sents from these targets:')
-        print(target_data[target_data.sent_id.isin(bad_ids)].target.unique())
+        print(target_data[target_data.sent_idx.isin(bad_ids)].target.unique())
         ids = good_ids
     sentence_data = sentence_data.loc[ids]
 
     sense_sents = []
     num_bad = 0
     for sent_id, row in tqdm(sentence_data.iterrows(), total=len(sentence_data)):
-        sent = row['word_index_sentence']
+        sent = row['word_idx_sent']
 
         sense_sent = []
         add_sent = True
@@ -59,16 +56,16 @@ def process_sentences(sentence_data, target_data, targets, ids):
 
     return sense_sents
 
-def print_sense_sents(sense_sents, output_path):
+def save_sense_sents(sense_sents, output_path, corpus_name):
     print(f'{len(sense_sents):,} sentences modified with senses')
 
-    sense_data = pd.DataFrame(sense_sents, columns=['sent_id', 'sense_sentence'])
-    sense_data.set_index('sent_id', inplace=True) 
+    sense_data = pd.DataFrame(sense_sents, columns=['sent_idx', 'sense_sent'])
+    sense_data.set_index('sent_idx', inplace=True) 
 
     Path(output_path).mkdir(parents=True, exist_ok=True)
-    sense_data.to_pickle(f'{output_path}/sense_sentences.pkl')
+    sense_data.to_pickle(f'{output_path}/{corpus_name}_sense_sentences.pkl')
 
-def create_sense_sentences(sentence_path, output_path, slice_max=None):
+def create_sense_sentences(sentence_path, output_path, corpus_name, slice_max=None):
     target_data = pd.read_pickle(
         f'{output_path}/target_sense_labels.pkl')
     print(f'{len(target_data):,} targets predicted')
@@ -76,13 +73,13 @@ def create_sense_sentences(sentence_path, output_path, slice_max=None):
     targets = list(target_data.target.unique())
     print(f'{len(targets)} targets selected')
     
-    ids = target_data.sent_id.unique()
+    ids = target_data.sent_idx.unique()
     print(f'{len(ids):,} unique sentences with assigned senses')
 
     if slice_max is None:
         sentence_data = get_sentence_data(sentence_path)
         sense_sents = process_sentences(sentence_data, target_data, targets, ids)
-        print_sense_sents(sense_sents, output_path)
+        save_sense_sents(sense_sents, output_path, corpus_name)
 
     else:
         for slice_num in range(0, slice_max):
@@ -92,4 +89,4 @@ def create_sense_sentences(sentence_path, output_path, slice_max=None):
             o_path = f'{output_path}/slice_{slice_num}'
             print(f'\n==== Slice {slice_num} ====')
             sense_sents = process_sentences(sentence_data, target_data, targets, ids)
-            print_sense_sents(sense_sents, o_path)
+            save_sense_sents(sense_sents, o_path, corpus_name)
